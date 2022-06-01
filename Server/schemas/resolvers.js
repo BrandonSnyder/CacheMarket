b38c8169184f458c0409357ad861dda7e275f01b
Fisher Davis <fisherd02@yahoo.com>
2022-06-01 14:18:20 -0400

    created user model and can create user with mutation

--- Server/schemas/resolvers.js
+++ Server/schemas/resolvers.js
@@ -1,11 +1,20 @@
-const { Item } = require("../models");
+const { Item, User } = require("../models");
 
 const resolvers = {
   Query: {
     item: async () => {
       return Item.find({});
     },
+    user: async() => {
+      return User.find({})
+    }
   },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      return { user }
+    }
+  }
 };
 
 module.exports = resolvers;
