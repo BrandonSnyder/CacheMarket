8077c6feb774498a0f7fa604e82a23000b775ac8
sirmojosodope <stoverlee29@gmail.com>
2022-06-01 10:15:46 -0400

    trying

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import Home from './pages/Home';
-
+import { Market } from "./pages/Market/index"
+ 
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -12,12 +12,14 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
+      <Market/>
       <Router>
         <div className="flex-column justify-center align-center min-100-vh bg-primary">
           <Routes>
             <Route 
               path="/" 
               element={<Home />}
+              
             />
           </Routes>
         </div>
