6f914a1a80ebe75700d2f10afdbbc905cbbd5566
Brandon Snyder <brandon.snyder019@gmail.com>
2022-06-02 09:24:04 -0400

    created signup form and edited css for signup, create post and liked items to look uniform

--- Client/src/App.js
+++ Client/src/App.js
@@ -4,9 +4,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 import Market from './pages/Market';
-import CreatePostFrom from './components/createPostForm/CreatePostForm'
-import LikedItems from './components/likedItems/LikedItems';
+import CreatePostFrom from './components/createPostForm/CreatePostForm.js'
+import LikedItems from './components/likedItems/LikedItems.js';
 import CacheNav from "./components/Navbar/Navbar.js";
+import Signup from './components/signup/Signup.js';
 
 const client = new ApolloClient({
   uri: "/graphql",
@@ -19,6 +20,8 @@ function App() {
     <ApolloProvider client={client}>
       <CacheNav />
       <Market/>
+      <Signup/>
+      <CreatePostFrom/>
       <Router>
         <div className="flex-column justify-center align-center min-100-vh bg-primary">
           <Routes>
