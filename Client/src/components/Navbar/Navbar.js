4bfaa60d1f45c7ebe4cc11610d95e6bdbbeb1681
Brandon Snyder <brandon.snyder019@gmail.com>
2022-06-04 20:16:15 -0400

    login working

--- Client/src/components/Navbar/Navbar.js
+++ Client/src/components/Navbar/Navbar.js
@@ -1,33 +1,77 @@
 import React from "react";
-
+import Auth from "../../utils/auth";
 import "./Navbar.css";
 import logo from "./fingerlogo2.png";
 
-
 function CacheNav() {
-  return (
-    <div>
-      <>
-        <nav className="navbar" variant="dark">
-          <section className="navbarContainer">
-          <div className="logoName">
-          <img src={logo} alt="logo" />
-            <h1 className="navbarTitle" href="">
-              CacheMarket
-            </h1>
-          </div>
-            <nav className=" navBarLinkContainer">
-              <a className="navbarLink" href="/">Home</a>
-              <a className="navbarLink" href="/login">Login</a>
-              <a className="navbarLink" href="/signup">Sign Up</a>
-              <a className="navbarLink" href="/contact-us">Contact Us</a>
-              <a className="navbarLink" href="/create-post">New Post</a>
-            </nav>
-          </section>
-        </nav>
-      </>
-    </div>
-  );
+  if (Auth.loggedIn()) {
+    return (
+      <div>
+        <>
+          <nav className="navbar" variant="dark">
+            <section className="navbarContainer">
+              <div className="logoName">
+                <img src={logo} alt="logo" />
+                <h1 className="navbarTitle" href="">
+                  CacheMarket
+                </h1>
+              </div>
+              <nav className=" navBarLinkContainer">
+                <a className="navbarLink" href="/">
+                  Home
+                </a>
+                
+                <a className="navbarLink" href="/contact-us">
+                  Contact Us
+                </a>
+                <a className="navbarLink" href="/create-post">
+                  New Post
+                </a>
+                <a className="navbarLink" onClick={() => Auth.logout()}  href="/">
+                  logout
+                </a>
+              </nav>
+            </section>
+          </nav>
+        </>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <>
+          <nav className="navbar" variant="dark">
+            <section className="navbarContainer">
+              <div className="logoName">
+                <img src={logo} alt="logo" />
+                <h1 className="navbarTitle" href="">
+                  CacheMarket
+                </h1>
+              </div>
+              <nav className=" navBarLinkContainer">
+                <a className="navbarLink" href="/">
+                  Home
+                </a>
+                <a className="navbarLink" href="/login">
+                  Login
+                </a>
+                <a className="navbarLink" href="/signup">
+                  Sign Up
+                </a>
+                <a className="navbarLink" href="/contact-us">
+                  Contact Us
+                </a>
+                <a className="navbarLink" href="/create-post">
+                  New Post
+                </a>
+                
+              </nav>
+            </section>
+          </nav>
+        </>
+      </div>
+    );
+  }
 }
 
 export default CacheNav;
--- Client/src/components/login/Login.js
+++ Client/src/components/login/Login.js
@@ -1,12 +1,52 @@
 import React, { useState } from "react";
-
+import Auth from '../../utils/auth';
 import "./Login.css";
+import { LOGIN } from "../../utils/mutations"; // verify create login
+import { useMutation } from "@apollo/client";
 
 function LoginForm() {
+  const [formState, setFormState] = useState({
+    email: "",
+    password: "",    
+  });
+  const [login, { error }] = useMutation(LOGIN);
+  
+  
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    event.preventDefault();
+      const  variable  = login({
+        variables: { 
+          email: formState.email,
+        password: formState.password,
+         },
+      });      
+      const token = variable;
+      Auth.login(token);
+      window.location.reload();
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    
+    if (name === "email" && value.length <= 280) {
+      setFormState({ ...formState, [name]: value });
+    } else if (name !== "email") {
+      setFormState({ ...formState, [name]: value });
+    }
+    if (name === "password" && value.length <= 280) {
+      setFormState({ ...formState, [name]: value });
+    } else if (name !== "password") {
+      setFormState({ ...formState, [name]: value });
+    }
+  };
+
+
+
   return (
     <section className="loginContainer">
       <h1>Login</h1>
-      <form className="loginForm">
+      <form className="loginForm" onSubmit={handleFormSubmit}>
         <div className="loginInput">
           <label htmlFor="email">
             Email:
@@ -16,6 +56,7 @@ function LoginForm() {
             name="email"
             type="text"
             id="email"
+            onChange={handleChange}
           />
         </div>
         <div className="loginInput">
@@ -27,8 +68,14 @@ function LoginForm() {
             name="password"
             type="text"
             id="password"
+            onChange={handleChange}
           />
         </div>
+        {error ? (
+          <div>
+            <p className="error-text">The provided credentials are incorrect</p>
+          </div>
+        ) : null}
         <button className="submitbtn" type="submit">
           Submit
         </button>
