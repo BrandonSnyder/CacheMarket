72409b21c4441a628592d579b7f49d0c39a727fe
Brandon Snyder <brandon.snyder019@gmail.com>
2022-06-09 14:14:28 -0400

    deploy

--- Server/config/connection.js
+++ Server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-
+MONGODB_URI='mongodb+srv://bsnyd008:FXD9ig2pqS7rrgAr@cachemarket.snwkygt.mongodb.net/?retryWrites=true&w=majority'
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/CacheMarketDB",
